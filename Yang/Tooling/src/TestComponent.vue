d5dbd6c40687e4a1c840f119ae5de0380acf99e8
Freddy Yang <freddy10100@hotmail.com>
2018-12-11 11:42:33 +0800

    Add files via upload

---
+++ Yang/Tooling/src/TestComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="message">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  data () {
+    return {
+      message: 'Halo world.'
+    }
+  }
+}
+</script>
